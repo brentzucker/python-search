a7604b2d24296aa2c1dbefb6cae60c89e689969c
brentzucker <brentzucker@gmail.com>
2015-01-17 15:13:06 -0500

    Twitter search module
    Created a twit.py, it authenticates with twitter, gets the api handler,
    and has a tweet_search function that queries twitter and returns a list
    of results

---
+++ twit.py
@@ -0,0 +1,30 @@
+import tweepy
+
+#returns tweets that match sepcified query 
+
+#Authentication details
+consumer_key = 'CZqpg8KRh3Y7aWCXf9tj6N9D3'
+consumer_secret = 'fOkmAxKf7QcIHnUDzN5CPgvPONtqVpDfbtdL5YGab37BQGfmoP'
+access_token = '17464727-cD9R5EdeBteo55MDq2wW0E1eUlXv3FzhswpZ0uMrx'
+access_token_secret = '1vsT3ceCiCDMSAiZHpn2bJsRTRALJdFHukZcgok8oaVuq'
+
+#Create authenication token using our details
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+#Get API handler
+api = tweepy.API(auth)
+
+#Loops through search_results printing each element
+#for tweet in search_results:
+#	print tweet.text
+
+def tweet_search(query):
+	#Stores search results in list
+	search_results = api.search(query)
+
+	results = []
+	for result in search_results:
+		results.append(result.text)
+
+	return results
